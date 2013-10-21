ad328dd0cd80a6ace05258915e68e4dd9b9d900f
Ben Nolan <ben@nolanconsul.com>
2013-10-22 10:57:50 +1300

    Screenshots.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131021061825) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "legs", force: true do |t|
     t.string   "city_path"
     t.float    "latitude"
